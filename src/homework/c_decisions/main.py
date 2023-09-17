f447c5e92bd12a5133c7d241bfb213c5c9267510
spookylights <djones314159@gmail.com>
2023-09-17 01:57:10 +0000

    homework 2 boolean logic decision processing

--- src/homework/c_decisions/main.py
+++ src/homework/c_decisions/main.py
@@ -1,1 +1,9 @@
-#
+import decisions
+
+option = float(input("Please input a value for options: "))
+
+total = float(input("Please input a value for total: "))
+
+result = decisions.get_options_ratio(option, total)
+
+print(decisions.get_faculty_rating(result))
