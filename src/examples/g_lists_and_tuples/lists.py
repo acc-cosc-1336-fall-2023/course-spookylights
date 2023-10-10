c2bd0cd1955560c5ef1d3885b4cc68a065ea2e4f
Dustin Jones <djones314159@gmail.com>
2023-10-09 19:02:43 -0500

    practice lists, lists w loops

--- src/examples/g_lists_and_tuples/lists.py
+++ src/examples/g_lists_and_tuples/lists.py
@@ -1,2 +1,33 @@
-def test_config():
-    return True
+def display_list():
+    list1 = [5, 10, 20]
+    print(list1)
+
+def display_list_w_while():
+    list1 = [5, 10, 20]
+    i = 0
+    while i < len(list1):
+        print(list1[i])
+        i += 1
+
+def display_reverse_list_w_while():
+    list1 = [5, 10, 20]
+    i = len(list1) - 1
+    while i >= 0:
+        print(list1[i])
+        i -= 1
+
+def display_list_w_for_range():
+    list1 = [5, 10, 20, 3, 7, 1, 50]
+    for i in range(0, len(list1)):
+        print(list1[i])
+
+def display_list_w_for_range_w_step():
+    list1 = [5, 10, 20, 3, 7, 1, 50]
+    for i in range(0, len(list1), 2):
+        print(list1[i])
+
+def display_list_w_for_range_reverse():
+    list1 = [5, 10, 20]
+    for i in range(len(list1), 0, -1):
+        
+        print(list1[i-1])
--- src/examples/g_lists_and_tuples/main.py
+++ src/examples/g_lists_and_tuples/main.py
@@ -1,1 +1,13 @@
-#main program
+import lists
+
+#lists.display_list()
+
+#lists.display_list_w_while()
+
+#lists.display_reverse_list_w_while()
+
+lists.display_list_w_for_range()
+print("--------------------")
+lists.display_list_w_for_range_w_step()
+print("--------------------")
+lists.display_list_w_for_range_reverse()
