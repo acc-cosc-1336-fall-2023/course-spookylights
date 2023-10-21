3a2bc43c6b4303367265f2fe24eee89d0ee08e40
Dustin Jones <djones314159@gmail.com>
2023-10-21 18:06:55 -0500

    list and tuples slices iteration

--- src/examples/g_lists_and_tuples/lists.py
+++ src/examples/g_lists_and_tuples/lists.py
@@ -1,3 +1,6 @@
+def test_config():
+    return True
+
 def display_list():
     list1 = [5, 10, 20]
     print(list1)
@@ -31,3 +34,39 @@ def display_list_w_for_range_reverse():
     for i in range(len(list1), 0, -1):
         
         print(list1[i-1])
+
+def get_list_total_while(list1):
+    index = 0
+    sum = 0
+
+    while (index < len(list1)):
+        sum += list1[index]
+        index += 1
+
+    return sum
+
+def get_list_total_for(list1):
+    sum = 0
+
+    for i in range(0, len(list1)):
+        sum += list1[i]
+
+    return sum
+
+def list_reference_parameter(list):
+    list[0] = 0
+
+def get_list_return_value(list):
+    list[0] = 0
+
+    return list
+
+def list_inside_list():
+    list1 = [[5, 10, 20], [6, 10, 5], [5, 0, 20]]
+    print(list1[0][0])
+    list_at_index_0 = list1[0]
+    print(list_at_index_0[1])
+    list_at_index_2 = list1[2]
+    print(list_at_index_2[2])
+    
+
--- tests/examples/g_lists_and_tuples/tests_lists_and_tuples.py
+++ tests/examples/g_lists_and_tuples/tests_lists_and_tuples.py
@@ -1,9 +1,29 @@
 import unittest
 
-from src.examples.g_lists_and_tuples.lists import test_config
+from src.examples.g_lists_and_tuples.lists import get_list_return_value, get_list_total_for, get_list_total_while, list_reference_parameter, test_config
 
 class Test_Config(unittest.TestCase):
 
-    def test_configuration(self):
-        self.assertEqual(True, test_config())
+    def test_config(self):
+        self.assertEqual(test_config(), True)
 
+    def test_get_total_w_while(self):
+        list1 = [5, 10, 20]
+        self.assertEqual(get_list_total_while(list1), 35)
+
+    def test_get_list_total_for(self):
+        list1 = [5, 10, 20]
+        self.assertEqual(get_list_total_for(list1), 35)
+
+    def test_list_ref_param_not_a_copy(self):
+        list = [5, 10, 20]
+        list_reference_parameter(list)
+
+        self.assertEqual(list, [0, 10, 20])
+
+    def test_list_return_value(self):
+        list = [5, 10, 20]
+        print("before", id(list))
+        get_list_return_value(list)
+        print("after", id(list))
+        self.assertEqual(list, [0, 10, 20])
